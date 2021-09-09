494ddd163aef55136b3cf2aeda79d1298a02233a
Bad_Marat <badamshin1975@yandex.ru>
2021-09-09 21:31:04 +0300

    Создание структуры и вывод меню

--- WorkOfBadamshin/WorkOfBadamshin/WorkOfBadamshin.cpp
+++ WorkOfBadamshin/WorkOfBadamshin/WorkOfBadamshin.cpp
@@ -12,7 +12,8 @@ void PrintMenu()
 		<< "5. Редактировать КС" << endl
 		<< "6. Сохранить" << endl
 		<< "7. Загрузить" << endl
-		<< "0. Выход" << endl;
+		<< "0. Выход" << endl
+		<< "Нажмите на одну из клавиш чисел на клавиатуре: ";
 };
 
 // Структуры Трубы и КС
@@ -33,8 +34,51 @@ struct CompressorStation
 	int effectiveness;
 };
 
+
 int main()
 {
-	setlocale(LC_ALL, "Rus");
-	PrintMenu();
+	int command;
+	setlocale(LC_ALL, "Russian");
+	while (true)
+	{
+		PrintMenu();
+		cin >> command;
+		switch (command)
+		{
+		case 1:
+		{
+			break;
+		}
+		case 2:
+		{
+			break;
+		}
+		case 3:
+		{
+			break;
+		}
+		case 4:
+		{
+			break;
+		}
+		case 5:
+		{
+			break;
+		}
+		case 6:
+		{
+			break;
+		}
+		case 7:
+		{
+			break;
+		}
+		case 0:
+		{
+			return 0;
+		}
+		default:
+			cout << "Вы ввели неправильные данные!!!" << endl;
+		}
+	}
 }
