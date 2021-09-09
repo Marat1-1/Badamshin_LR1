075e247865db874debd5e859b70f5044e5f9cac8
Bad_Marat <badamshin1975@yandex.ru>
2021-09-09 16:48:05 +0300

    Добавил структуры трубы и КС

--- WorkOfBadamshin/WorkOfBadamshin/WorkOfBadamshin.cpp
+++ WorkOfBadamshin/WorkOfBadamshin/WorkOfBadamshin.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 using namespace std;
 
+// Вывод меню на консоль
 void PrintMenu()
 {
 	cout << "Перед вами меню программы, выберите одну из операций!" << endl
@@ -12,7 +13,26 @@ void PrintMenu()
 		<< "6. Сохранить" << endl
 		<< "7. Загрузить" << endl
 		<< "0. Выход" << endl;
-}
+};
+
+// Структуры Трубы и КС
+struct Pipe
+{
+	string id;
+	int length;
+	int diameter;
+	bool repair;
+};
+
+struct CompressorStation
+{
+	string id;
+	string name;
+	int countWorkShops;
+	int countWorkShopsInOperation;
+	int effectiveness;
+};
+
 int main()
 {
 	setlocale(LC_ALL, "Rus");
