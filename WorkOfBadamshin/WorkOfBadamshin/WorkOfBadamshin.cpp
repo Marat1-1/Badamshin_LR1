2e69b5a6d181c0bb1fd16f48148257e1a5c2688b
Bad_Marat <badamshin1975@yandex.ru>
2021-09-10 20:02:08 +0300

    Добавил первый пункт меню
    Пока что в нём не всё так гладко работает, дальше буду исправлять всё

--- WorkOfBadamshin/WorkOfBadamshin/WorkOfBadamshin.cpp
+++ WorkOfBadamshin/WorkOfBadamshin/WorkOfBadamshin.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
+#include <Windows.h>
+#include <limits>
 using namespace std;
 
 // Вывод меню на консоль
@@ -14,14 +17,25 @@ void PrintMenu()
 		<< "6. Сохранить" << endl
 		<< "7. Загрузить" << endl
 		<< "0. Выход" << endl
-		<< "Нажмите на одну из клавиш чисел на клавиатуре: ";
+		<< "Введите число, которое и будет являться командой для программы: ";
 };
 
 // Проверка корректного ввода данных для команды
 bool CheckCommand(string command)
 {
-	if (!isdigit(command[0]) || size(command) > 1)
+	if (!isdigit(command[0]) || command[0] > '7' || size(command) > 1)
 		return false;
+	return true;
+}
+
+// Проверка ввода числа
+bool CheckInputDigit(string number)
+{
+	for (size_t i = 0; i < size(number); i++)
+	{
+		if (!isdigit(number[i]) || number[i] != '.')
+			return false;
+	}
 	return true;
 }
 
@@ -29,12 +43,12 @@ bool CheckCommand(string command)
 struct Pipe
 {
 	string id;
-	int length;
-	int diameter;
+	double length;
+	double diameter;
 	bool repair;
 };
 
-//Структура КС
+// Структура КС
 struct CompressorStation
 {
 	string id;
@@ -44,21 +58,60 @@ struct CompressorStation
 	int effectiveness;
 };
 
+// Создание новой трубы
+Pipe NewPipe()
+{
+	Pipe result;
+	string strRepair;
+	cout << "Введите id трубы: ";
+	cin >> result.id;
+	cout << "Введите длину трубы: ";
+	cin >> result.length;
+	cout << "Введите диаметр трубы: ";
+	cin >> result.diameter;
+	cout << "Укажите находится ли труба в ремонте, если да, то введите \"y\", если же труба не в ремонте, введите \"n\": ";
+	while (true)
+	{
+		cin >> strRepair;
+		if (strRepair == "y")
+		{
+			result.repair = true;
+			break;
+		}
+		else if (strRepair == "n")
+		{
+			result.repair = false;
+			break;
+		}
+		else
+			cout << "Вы ввели что-то непонятное, повторите ввод по указанным выше правилам, \"y\" - это да, \"n\" - это нет: ";
+	}
+	return result;
+};
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	HANDLE myHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 	string command;
+	vector <Pipe> vectorPipes;
+	size_t countPipes = 0, countCompessorStation = 0;
 	while (true)
 	{
 		system("CLS");
+		SetConsoleTextAttribute(myHandle, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
 		PrintMenu();
-		cin >> command;
+		getline(cin, command);
 		if (CheckCommand(command))
 		{
 			switch (stoi(command))
 			{
 			case 1:
 			{
+				system("CLS");
+				cout << "ИНИЦИАЛИЗАЦИЯ ТРУБЫ" << endl;
+				vectorPipes.resize(++countPipes);
+				vectorPipes[countPipes - 1] = NewPipe();
 				break;
 			}
 			case 2:
@@ -93,8 +146,11 @@ int main()
 		}
 		else
 		{
-			cout << "Некорректный ввод данных!!!" << endl;
-			cin.get();
+			SetConsoleTextAttribute(myHandle, FOREGROUND_RED);
+			cout << "Некорректный ввод данных!!! Осуществите ввод заново!!!" << endl
+				<< "Сообщение пропадёт через 5 секунд" << endl;
+			Sleep(5000);
 		}
+		while ((getchar()) != '\n');
 	}
 }
